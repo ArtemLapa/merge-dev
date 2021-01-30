7c0b29501fecc4db9ca12dc6990e3c4087fb3e8e
ArtemLapa <artyom.lapa@gmail.com>
2021-01-30 18:03:01 +0200

    :tada: befin a project & setup settings

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <merge-home />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MergeHome from '@/components/MergeHome/MergeHome.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components: {
+    MergeHome,
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
---
+++ src/components/MergeHome/MergeHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div class="home__block">
+      <ul class="home__list">
+        <li class="home__items"></li>
+        <li class="home__items"></li>
+        <li class="home__items"></li>
+        <li class="home__items"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MergeHome',
+}
+</script>
+
+<style lang="scss">
+  $style: "home";
+  .#{$style} {
+    &__block {
+      background-color: aqua;
+      width: 900px;
+      height: 600px;
+      margin: auto;
+    }
+    &__list {
+      @include flex(space-between, center)
+    }
+    &__items {
+      width: 50px;
+      height: 50px;
+      background-color: $blue-100;
+    }
+  }
+</style>
